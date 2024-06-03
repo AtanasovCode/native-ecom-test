7cfccf738d5c3f122faf2905f6328a858ed944de
AtanasovCode <a_ljupce@yahoo.com>
2024-06-03 15:21:01 +0200

    initial router setup & created Home route

--- App.js
+++ App.js
@@ -1,44 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Image } from 'expo-image';
-import { LinearGradient } from 'expo-linear-gradient';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-export default function App() {
+//importing routes
+import Home from "./src/routes/Home";
+
+const App = () => {
+
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <LinearGradient
-        colors={['#232526', '#66686a']}
-        style={{ position: 'absolute', left: 0, right: 0, top: 0, bottom: 0 }}
-      />
-
-      <View style={{ position: 'absolute', bottom: 50, left: 0, right: 0, alignItems: 'center' }}>
-        <Image
-          style={{ width: 100, height: 100 }}
-          contentFit="contain"
-          source="https://raw.githubusercontent.com/expo/styleguide/main/common/logos/word-mark-logo.svg"
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen 
+          component={Home}
+          name="Home"
+          options={{
+            headerShown: false,
+          }}
         />
-      </View>
-
-      <Image
-        style={{ width: 200, height: 200 }}
-        contentFit="contain"
-        source="https://d33wubrfki0l68.cloudfront.net/554c3b0e09cf167f0281fda839a5433f2040b349/ecfc9/img/header_logo.svg"
-      />
-
-      <Text style={{ fontSize: 22, marginTop: 15, color: '#fff' }}>
-        New Architecture: <Text style={{ fontWeight: 'bold' }}>Enabled</Text>
-      </Text>
-
-      < StatusBar style="auto" />
-    </View>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
